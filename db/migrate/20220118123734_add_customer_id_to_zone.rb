0426d9d5b439dcba82023849db010bd117e4ecea
jirkanovak <jirkanovakneda11@seznam.cz>
2022-01-18 14:22:03 +0100

    asi dulezite

--- db/migrate/20220118123734_add_customer_id_to_zone.rb
+++
@@ -1,5 +0,0 @@
-class AddCustomerIdToZone < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :zones, :customer, foreign_key: true
-  end
-end
--- db/migrate/20220118123755_add_visitor_id_to_zone.rb
+++
@@ -1,5 +0,0 @@
-class AddVisitorIdToZone < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :zones, :visitor, foreign_key: true
-  end
-end
