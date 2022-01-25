fba0e0452790d5dd8371a0d386064fe054813e08
jirkanovak <jirkanovakneda11@seznam.cz>
2022-01-25 15:12:43 +0100

    validation for customers and visitors on zones

--- app/models/customers_zone_acce.rb
+++ app/models/customers_zone_acce.rb
@@ -2,4 +2,8 @@ class CustomersZoneAcce < ApplicationRecord
   audited
   belongs_to :customer
   belongs_to :zone
+
+
+  validates :zone_id, presence: true, uniqueness: { scope: :customer_id }
+
 end
--- app/models/visitors_zone_acce.rb
+++ app/models/visitors_zone_acce.rb
@@ -2,4 +2,7 @@ class VisitorsZoneAcce < ApplicationRecord
   audited
   belongs_to :visitor
   belongs_to :zone
+
+  validates :zone_id, presence: true, uniqueness: { scope: :visitor_id }
+
 end
