7b5b48abf2bc77a22f0f1ae45a1a6770dd3bbe5e
jirkanovak <jirkanovakneda11@seznam.cz>
2022-01-26 15:25:17 +0100

    User
    User creation completed show is missing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,36 @@
 class UsersController < ApplicationController
-  def index
-  end
+  before_action :set_user, only: %i[ show edit update destroy ]
 
-  def create
+
+  def index
     @user = User.new
   end
+
+
+  def create
+  @user = User.new(user_params)
+
+  respond_to do |format|
+    if @user.save
+        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
+        format.json { render :show, status: :created, location: @user }
+    else
+        format.html { render :index, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+    end
+  end
+  end
+
+
+  private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:phone, :email, :password, :account_level, :name )
+  end
+
+
 end
